e673ad17befa94d75c620d067fc0b98399b1c580
Stanley Hayford <hayfordstanley@gmail.com>
2022-08-17 00:52:26 +0000

    Minor change

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -16,7 +16,7 @@ const Layout = ({children}) => {
                 backgroundColor: 'background.paper',
                 flexDirection: 'column'
             }}>
-                <Box sx={{mb: 6}}>
+                <Box sx={{mb: 7}}>
                     <Header/>
                 </Box>
                 <Box sx={{flex: 1}}>
